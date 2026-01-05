2597ae937814d8eb8373e88baeec150f248f9a42
sunss <oooooohoney@163.com>
2026-01-05 20:04:49 +0800

    fix: avoid home prerender during build

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -4,6 +4,8 @@ import { getPromptOptions } from "@/lib/data/prompts";
 import HomeGenerateWorkbench from "./_components/home-generate-workbench";
 import { HomePromptHighlights } from "./_components/home-prompt-highlights";
 
+export const dynamic = "force-dynamic";
+
 export default async function Home() {
   const prompts = await getPromptOptions();
   const models = await getModelConfigs();
